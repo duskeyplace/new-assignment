c8229687712cb8c68c7bfeab6c6bc6c71f053b39
“[ameer <[hamza427133@gmail.com]>
2024-03-23 06:38:40 +0500

    new asgnment

---
+++ app.js
@@ -0,0 +1,54 @@
+// Q1
+
+
+// var firstName = prompt("Enter your first name:");
+// var lastName = prompt("Enter your last name:");
+
+// var fullName = firstName + " " + lastName;
+
+
+// alert("Hello, " + fullName + "! Welcome!");
+
+
+
+
+// Q2
+
+
+// var favoriteModel = ("My favorite phone is: Sumsung Galaxy Ultra S24 ")
+
+// document.write(favoriteModel)
+
+
+
+
+// Q3
+
+
+// var word = "Pakistani";
+
+
+// var index = word.indexOf("n");
+
+
+// document.write("The index of letter 'n' in the word 'Pakistani' is: " + index);
+
+
+// Q4
+
+
+
+// var word = "Hello World";
+
+// var lastIndex = word.lastIndexOf("l");
+
+
+// document.write("The last index of letter 'l' in the word 'Hello World' is: " + lastIndex);
+
+
+
+
+
+// Q5
+
+
